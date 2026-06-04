f96c21de0951940077a43707bf5bd546df6231e6
mykola-lp <mykola.sukhorab@gmail.com>
2026-06-04 21:25:50 +0300

    SQL statment check

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http"
 import * as schema from "./schema"
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema })
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+// prints every SQL statement and its parameters to the console
+//   logger: true,
+})
