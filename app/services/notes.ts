ab048329cf2d756b95b5ee6f61fc762f352f5a02
mykola-lp <mykola.sukhorab@gmail.com>
2026-06-04 14:09:33 +0300

    Add notes

---
+++ app/services/notes.ts
@@ -0,0 +1,34 @@
+import { eq } from "drizzle-orm"
+import { db } from "../../db"
+import { notes } from "../../db/schema"
+
+export const getNotes = async (importantOnly: boolean) => {
+  if (importantOnly) {
+    return db.query.notes.findMany({
+      where: eq(notes.important, true),
+    })
+  }
+  
+  return db.query.notes.findMany()
+}
+
+export const getNoteById = async (id: number) => {
+  return db.query.notes.findFirst({
+    where: eq(notes.id, id),
+  })
+}
+
+export const addNote = async (content: string, important: boolean) => {
+  await db.insert(notes).values({ content, important })
+}
+
+export const toggleImportance = async (id: number) => {
+  const note = await getNoteById(id)
+
+  if (note) {
+    await db
+      .update(notes)
+      .set({ important: !note.important })
+      .where(eq(notes.id, id))
+  }
+}
