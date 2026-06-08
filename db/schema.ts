ed219d1e8f437f9a0e515b4bd32a2cc0dc44438c
mykola-lp <mykola.sukhorab@gmail.com>
2026-06-08 15:32:19 +0300

    fix(db): remove default empty password hash

--- db/schema.ts
+++ db/schema.ts
@@ -21,7 +21,7 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   username: text("username").notNull().unique(),
   name: text("name").notNull(),
-  passwordHash: text("password_hash").notNull().default(""),
+  passwordHash: text("password_hash").notNull(),
 })
 
 export const usersRelations = relations(users, ({ many }) => ({
