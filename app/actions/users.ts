feb242ae5dda1870571e38e1be409418d9b06d11
mykola-lp <mykola.sukhorab@gmail.com>
2026-06-10 12:18:43 +0300

    feat: add fields validations on server side

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -8,12 +8,24 @@ import { createUser } from "@/app/services/users"
 import type { RegisterState } from "./users.types"
 
 export async function registerUser(
-  _prevState: RegisterState,
+  prevState: RegisterState,
   formData: FormData,
 ): Promise<RegisterState | never> {
   const username = formData.get("username") as string
   const name = formData.get("name") as string
   const password = formData.get("password") as string
+
+  if (!username || username.length <= 4) {
+    return { error: "Username must be at least 4 characters long" }
+  }
+
+  if (!name || name.length <= 4) {
+    return { error: "Name must be at least 4 characters long" }
+  }
+
+  if (!password || password.length <= 4) {
+    return { error: "Password must be at least 4 characters long" }
+  }
 
   try {
     const passwordHash = await bcrypt.hash(password, 10)
