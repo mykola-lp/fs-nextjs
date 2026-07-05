d3e483ba2a46f3c3c0f120e04a1f398103931eca
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-05 16:14:18 +0300

    fix: add generate token button test id for e2e tests

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -172,6 +172,7 @@ const MePage = async () => {
       <form action={generateToken}>
         <button
           type="submit"
+          data-testid="generate-token-button"
           className="border rounded px-4 py-2 hover:bg-gray-50 cursor-pointer"
         >
           Generate token
