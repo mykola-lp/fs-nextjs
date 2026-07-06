2670f275b5151a646124e87c0bd07d76eb4b6319
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-06 10:24:04 +0300

    fix: fix Next.js image aspect ratio warning

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -7,6 +7,7 @@ const Profile = () => {
         src="/profile.jpg"
         width={200}
         height={200}
+        className="w-[200px] h-[200px]"
         alt="User profile"
       />
     </div>
